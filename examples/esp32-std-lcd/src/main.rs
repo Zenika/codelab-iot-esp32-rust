188c75c2868f29c7fd61b97130048ea89d17e7b3
Emeric Martineau <11473190+emeric-martineau@users.noreply.github.com>
2023-11-07 14:35:55 +0100

    Rewrite LCD example

--- examples/esp32-std-lcd/src/main.rs
+++ examples/esp32-std-lcd/src/main.rs
@@ -4,33 +4,38 @@ use std::thread;
 use std::time::Duration;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::i2c::I2cDriver;
+use embedded_hal::blocking::{i2c, delay::DelayMs};
 
 // LCD
-use esp_idf_hal::i2c;
+use liquidcrystal_i2c_rs::Lcd;
 
-mod lcd;
 
 // implement HAL...
-struct HW<'d> {
+struct I2cEsp32Driver<'d> {
   i2c: I2cDriver<'d>,
-  address: u8,
 }
+
+type I2cError = std::io::Error;
 
 // Implement the `Hardware` trait to give access to LCD pins
-impl lcd::I2C for HW<'_> {
-  fn write(&mut self, data: u8) -> Result<usize, lcd::I2cError> {
-    if let Ok(()) = self.i2c.write(self.address, &[data], 1000) {
-      return Ok(1);
-    }
+impl i2c::Write for I2cEsp32Driver<'_> {
+    type Error = I2cError;
 
-    return Err(lcd::I2cError::Io);
-  }
+    fn write(&mut self, address: u8, data: &[u8]) -> Result<(), Self::Error> {
+      if let Ok(()) = self.i2c.write(address, data, 1000) {
+        return Ok(());
+      }
+  
+      return Err(Self::Error::new(std::io::ErrorKind::Other, "Error to write data"));
+    }
 }
 
+struct DelayEsp32Driver { }
+
 // Implement the `Delay` trait to allow library to sleep for the given amount of time
-impl lcd::Delay for HW<'_> {
-  fn delay_us(&mut self, delay_usec: u32) {
-    thread::sleep(Duration::from_micros(delay_usec.into()));
+impl DelayMs<u8> for DelayEsp32Driver {
+  fn delay_ms(&mut self, delay_usec: u8) {
+    thread::sleep(Duration::from_millis(delay_usec.into()));
   }
 }
 
@@ -46,23 +51,24 @@ fn main() {
     let scl = peripherals.pins.gpio22;
     let sda = peripherals.pins.gpio21;
 
-    let i2c_config = i2c::config::Config::new()
+    let i2c_config = esp_idf_hal::i2c::config::Config::new()
       .baudrate(KiloHertz(100).into())
       .scl_enable_pullup(true)
       .sda_enable_pullup(true);
 
-    let i2c_driver = i2c::I2cDriver::new(peripherals.i2c0, sda, scl, &i2c_config).unwrap();
+    let i2c_driver = esp_idf_hal::i2c::I2cDriver::new(peripherals.i2c0, sda, scl, &i2c_config).unwrap();
 
-    let hw = HW { 
+    let mut i2c_esp32_driver = I2cEsp32Driver { 
       i2c: i2c_driver,
-      address: 0x27,
-     };
+    };
+
+    let mut delay_esp32_driver = DelayEsp32Driver {};
  
-    let mut lcd_screen = lcd::Lcd::new(hw).unwrap();
+    let mut lcd_screen = Lcd::new(&mut i2c_esp32_driver, 0x27, &mut delay_esp32_driver).unwrap();
 
     lcd_screen.clear().unwrap();
-    lcd_screen.set_display(lcd::Display::On).unwrap();
-    lcd_screen.set_backlight(lcd::Backlight::On).unwrap();
+    lcd_screen.set_display(liquidcrystal_i2c_rs::Display::On).unwrap();
+    lcd_screen.set_backlight(liquidcrystal_i2c_rs::Backlight::On).unwrap();
     lcd_screen.print("Hello 11").unwrap();
 
     loop {
